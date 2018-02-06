2a04d71c9358faaa45647e41e42618f3a0c2579b
buzzjmd <buzzjmd@gmail.com>
2018-02-06 12:19:21 +0000

    Install pytest-cov into dev env, and add test to get 100% coverage
    - Add test_keldcli_as_module() to test __main__.py

--- tests/test_keldcli.py
+++ tests/test_keldcli.py
@@ -5,3 +5,9 @@ import keldcli
 
 def test_keldcli():
     assert keldcli.cli() is None
+
+
+def test_keldcli_as_module():
+    import subprocess
+    returncode = subprocess.call(['python', '-m', 'keldcli'])
+    assert(returncode == 0)
