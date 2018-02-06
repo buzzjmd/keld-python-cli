bb668cb07da65d5abad791d8c949fa5278e3be28
buzzjmd <buzzjmd@gmail.com>
2018-02-06 20:53:13 +0000

    New test test_help_option()
    Click automatically creates help, so test it

--- tests/test_keldcli.py
+++ tests/test_keldcli.py
@@ -22,3 +22,12 @@ def test_keldcli_as_script():
     import subprocess
     returncode = subprocess.call(['keld'])
     assert(returncode == 0)
+
+
+def test_help_option():
+    """Test the --help option."""
+    runner = CliRunner()
+    help_result = runner.invoke(cli, ['--help'])
+    assert help_result.exit_code == 0
+    print(help_result.output)
+    assert '--help  Show this message and exit.' in help_result.output
