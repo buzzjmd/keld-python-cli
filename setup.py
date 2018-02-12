d52f3828b0a54585272478a43fe29970c6eabd08
buzzjmd <buzzjmd@gmail.com>
2018-02-12 11:25:05 +0000

    Install click in setup.py
    - Remove click from Pipfile. Install it as install_required in setup.py)
    - Remove flake8 from Pipfile dev env (it will be run from tox)
    - Tidyup tox.ini

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 
+"""The setup script."""
+
 from setuptools import setup, find_packages
 
+setup_requirements = [
+    'setuptools_scm',
+]
+
+requirements = [
+    'Click>=6.0',
+]
+
+console_script_entry_points = [
+    'keld=keldcli:cli'
+]
 
 setup(
     name='keldcli',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    setup_requires=setup_requirements,
+    install_requires=requirements,
     entry_points={
-        'console_scripts': [
-            'keld=keldcli:cli'
-        ]
+        'console_scripts': console_script_entry_points
     },
 )
