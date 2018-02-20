7422c5bec7c0c600f4197884de1fe66cb17a11c8
buzzjmd <buzzjmd@gmail.com>
2018-02-20 09:09:53 +0000

    Remove brackets from assert statements
    assert is a keyword and should not be called like a function.

--- tests/test_import_all.py
+++ tests/test_import_all.py
@@ -9,4 +9,4 @@ from keldcli import *
 
 def test_import_all():
     """Test what has been included in the __all__ variable of __init__.py"""
-    assert(keldcli.cli is cli)
+    assert keldcli.cli is cli
--- tests/test_keldcli.py
+++ tests/test_keldcli.py
@@ -7,11 +7,11 @@ def test_keldcli_as_module():
     """Test that the package can be run as a module (`python -m keldcli`)."""
     import subprocess
     returncode = subprocess.call(['python', '-m', 'keldcli'])
-    assert(returncode == 0)
+    assert returncode == 0
 
 
 def test_keldcli_as_script():
     """Test the package console script has been installed."""
     import subprocess
     returncode = subprocess.call(['keld'])
-    assert(returncode == 0)
+    assert returncode == 0
