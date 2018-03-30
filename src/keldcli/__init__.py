d4c324738ea02876705b68579022bbf4392e8943
buzzjmd <buzzjmd@gmail.com>
2018-03-30 11:25:36 +0100

    Add __distribution__ to interface and improve test_import_all.py

--- src/keldcli/__init__.py
+++ src/keldcli/__init__.py
@@ -2,6 +2,10 @@
 
 '''Command Line Interface.'''
 
+from .__about__ import __title__, __version__, __distribution__
 from .cli import cli
 
-__all__ = ['cli']
+__all__ = [
+    '__title__', '__version__', '__distribution__',
+    'cli',
+]
--- tests/test_import_all.py
+++ tests/test_import_all.py
@@ -8,4 +8,12 @@ from keldcli import *
 
 def test_import_all():
     """Test what has been included in the __all__ variable of __init__.py"""
+    assert cli
     assert keldcli.cli is cli
+    assert __distribution__
+    assert keldcli.__distribution__ is __distribution__
+    assert __title__
+    assert keldcli.__title__ is __title__
+    assert __title__ == "keldcli"
+    assert __version__
+    assert keldcli.__version__ is __version__
